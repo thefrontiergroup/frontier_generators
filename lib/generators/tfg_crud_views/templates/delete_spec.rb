1039e39cc19e55dc0e1923330d648d745bf302c8
Jordan Maguire <jmaguire@thefrontiergroup.com.au>
2015-05-28 11:54:45 +0800

    Primary attribute will be shown in alert

--- lib/generators/tfg_crud_views/templates/delete_spec.rb
+++ lib/generators/tfg_crud_views/templates/delete_spec.rb
@@ -17,6 +17,8 @@ feature 'Admin can delete an existing <%= model_configuration.as_constant %>' do
     # Ensure object is deleted
     expect(<%= model_configuration.as_constant %>.count).to eq(0)
     expect(page).to have_flash(:notice)
-    expect(page).not_to have_content(target_object.<%= model_configuration.primary_attribute.name %>)
+    within("table") do
+      expect(page).not_to have_content(target_object.<%= model_configuration.primary_attribute.name %>)
+    end
   end
 end
