8751bc50344c974b3f93cf5292863dd1f5094f94
Jordan Maguire <jmaguire@thefrontiergroup.com.au>
2015-06-11 21:09:59 +0800

    0.4.0
    Add in address specific factory declarations

--- lib/model_configuration/attribute/factory_declaration.rb
+++ lib/model_configuration/attribute/factory_declaration.rb
@@ -46,13 +46,23 @@ private
   end
 
   def string_data
-    if attribute.name =~ /name/
-      "FFaker::Name.name"
+    if attribute.name =~ /city/
+      "FFaker::AddressAU.city"
     elsif attribute.name =~ /email/
       "FFaker::Internet.email"
+    elsif attribute.name =~ /line_1/
+      "FFaker::AddressAU.street_address"
+    elsif attribute.name =~ /line_2/
+      "FFaker::AddressAU.secondary_address"
+    elsif attribute.name =~ /name/
+      "FFaker::Name.name"
     # Guessing since this is a string, it would be phone_number or mobile_number
     elsif attribute.name =~ /number/
       "FFaker::PhoneNumberAU.phone_number"
+    elsif attribute.name =~ /postcode/ || attribute.name =~ /post_code/
+      "FFaker::AddressAU.postcode"
+    elsif attribute.name =~ /suburb/
+      "FFaker::AddressAU.suburb"
     else
       "FFaker::Company.bs"
     end
