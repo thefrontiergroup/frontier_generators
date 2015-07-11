0584fd769c56375f14940ea9fe322998df34990c
Jordan Maguire <jmaguire@thefrontiergroup.com.au>
2015-07-11 15:19:17 +0800

    The form will input associations as f.association

--- lib/support/input/association.rb
+++ lib/support/input/association.rb
@@ -1,10 +1,14 @@
 require_relative 'attribute.rb'
 
-class Frontier::Input::Association < Frontier::Input::Attribute
+class Frontier::Input::Association < Frontier::Input
 
   def to_s(options={})
     options = options.merge({collection: "#{association.association_class}.all"})
-    super
+    # Should convert attribute "state" into:
+    #   f.association :state_id, collection: State.all
+    # With additional options as above you'd get:
+    #   f.association :state_id, abacus: 666, collection: State.all, one: :two
+    ["f.association #{attribute.as_field_name}", *input_options(options)].join(", ")
   end
 
   alias association attribute
