cde887c672c2e0994917baf0907ae71aeb8a49c7
Jordan Maguire <jmaguire@thefrontiergroup.com.au>
2016-01-04 22:18:17 +0800

    Add error handling for nil or incorrect file path

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,16 @@
 require 'bundler'
 Bundler.require
 require 'rails/all'
+require 'rails/generators'
 
 require './lib/frontier'
 
 def build_model_configuration
-  test_model_path = File.join("spec", "support", "test_model.yaml")
   ModelConfiguration::YamlParser.new(test_model_path).model_configuration
+end
+
+def test_model_path
+  File.join("spec", "support", "test_model.yaml")
 end
 
 RSpec.configure do |config|
