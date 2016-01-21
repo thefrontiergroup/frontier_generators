71ccf33f96455d0b0df99563472df94527a78c39
Jordan Maguire <jmaguire@thefrontiergroup.com.au>
2016-01-21 08:54:30 +0800

    Bug Fix: Ensure policies aren't generated when using CanCanCan

--- lib/generators/frontier_policy/frontier_policy_generator.rb
+++ lib/generators/frontier_policy/frontier_policy_generator.rb
@@ -4,7 +4,7 @@ class FrontierPolicyGenerator < Frontier::Generator
   source_root File.expand_path('../templates', __FILE__)
 
   def scaffold
-    unless model_configuration.skip_ui? || model_configuration.skip_policies?
+    if using_pundit? && !model_configuration.skip_ui? && !model_configuration.skip_policies?
       template "policy.rb", policy_path
       template "policy_spec.rb", policy_spec_path
     end
