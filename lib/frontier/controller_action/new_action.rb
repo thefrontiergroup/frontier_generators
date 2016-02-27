5f6c5634cabdb7b7c406e95ce6fb5c1419912f4f
Jordan Maguire <jmaguire@thefrontiergroup.com.au>
2016-02-27 23:15:59 +0800

    The new action will build any nested associations to pre-populate form

--- lib/frontier/controller_action/new_action.rb
+++ lib/frontier/controller_action/new_action.rb
@@ -6,14 +6,44 @@ class Frontier::ControllerAction::NewAction
     @model_configuration = model_configuration
   end
 
+  # def new
+  #   @model_name = ModelName.new
+  #   authorize!(@model_name)
+  #   @model_name.build_address # if nested, we need to pre-populate the form
+  #   @model_name.address.build_state # if deeply nested
+  # end
   def to_s
-    raw = <<-STRING
-def new
-  #{model_configuration.as_ivar_instance} = #{model_configuration.as_constant}.new
-  #{Frontier::Authorization::Assertion.new(model_configuration, :new).to_s}
-end
-STRING
-    raw.rstrip
+    ["def new", indented_action_contents, "end"].join("\n")
+  end
+
+private
+
+  # @model_name.build_address # or
+  # @model_name.address.build_state
+  def build_association_model_for(association, prefix)
+    "#{prefix || model_configuration.as_ivar_instance}.build_#{association.name}"
+  end
+
+  def build_association_models(model_configuration_or_association, prefix=nil)
+    nested_associations = model_configuration_or_association.attributes.select {|attribute| attribute.is_association? && attribute.is_nested?}
+    nested_associations.flat_map do |association|
+      # Accomodate shallow nested associations
+      [build_association_model_for(association, prefix)] +
+      # Accomodate deeply nested associations
+      build_association_models(association, "#{model_configuration.as_ivar_instance}.#{association.name}")
+    end
+  end
+
+  def action_contents
+    [
+      "#{model_configuration.as_ivar_instance} = #{model_configuration.as_constant}.new",
+      Frontier::Authorization::Assertion.new(model_configuration, :new).to_s,
+      build_association_models(model_configuration)
+    ].flatten.compact
+  end
+
+  def indented_action_contents
+    action_contents.map {|loc| "  #{loc}"}.join("\n")
   end
 
 end
--- spec/lib/frontier/controller_action/new_action_spec.rb
+++ spec/lib/frontier/controller_action/new_action_spec.rb
@@ -4,19 +4,99 @@ RSpec.describe Frontier::ControllerAction::NewAction do
 
   describe "#to_s" do
     subject { Frontier::ControllerAction::NewAction.new(model_configuration).to_s }
-    let(:model_configuration) { build_model_configuration }
 
-    let(:expected) do
-      raw = <<-STRING
+    context "a model without any associations" do
+      let(:model_configuration) { build_model_configuration }
+
+      let(:expected) do
+        raw = <<-STRING
 def new
   @test_model = TestModel.new
   authorize(TestModel)
 end
 STRING
-      raw.rstrip
+        raw.rstrip
+      end
+
+      it { should eq(expected) }
     end
 
-    it { should eq(expected) }
+    context "a model with shallow nested associations" do
+      let(:model_configuration) do
+        Frontier::ModelConfiguration.new({
+          test_model: {
+            attributes: {
+              address: {type: "belongs_to", form_type: "select"},
+              other_address: {
+                class_name: "Address",
+                type: "belongs_to",
+                form_type: "inline",
+                attributes: {
+                  line_1: {type: "string"},
+                  state: {type: "belongs_to", form_type: "select"}
+                }
+              },
+              name: {type: "string"},
+            }
+          }
+        })
+      end
+
+      let(:expected) do
+        raw = <<-STRING
+def new
+  @test_model = TestModel.new
+  authorize(TestModel)
+  @test_model.build_other_address
+end
+STRING
+        raw.rstrip
+      end
+
+      it { should eq(expected) }
+    end
+
+    context "a model with deeply nested associations" do
+      let(:model_configuration) do
+        Frontier::ModelConfiguration.new({
+          test_model: {
+            attributes: {
+              address: {type: "belongs_to", form_type: "select"},
+              other_address: {
+                class_name: "Address",
+                type: "belongs_to",
+                form_type: "inline",
+                attributes: {
+                  line_1: {type: "string"},
+                  state: {
+                    type: "belongs_to",
+                    form_type: "inline",
+                    attributes: {
+                      name: {type: "string"}
+                    }
+                  }
+                }
+              },
+              name: {type: "string"},
+            }
+          }
+        })
+      end
+
+      let(:expected) do
+        raw = <<-STRING
+def new
+  @test_model = TestModel.new
+  authorize(TestModel)
+  @test_model.build_other_address
+  @test_model.other_address.build_state
+end
+STRING
+        raw.rstrip
+      end
+
+      it { should eq(expected) }
+    end
   end
 
 end
