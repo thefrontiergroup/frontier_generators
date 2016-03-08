ea13a7ad3b52299cad667dd9d4715f71580642b9
Jordan Maguire <jmaguire@thefrontiergroup.com.au>
2016-03-08 21:51:04 +0800

    Add specs to demonstrate strong_params method

---
+++ lib/frontier/controller_action/strong_params_method.rb
@@ -0,0 +1,13 @@
+class Frontier::ControllerAction::StrongParamsMethod
+
+  attr_reader :model_configuration
+
+  def initialize(model_configuration)
+    @model_configuration = model_configuration
+  end
+
+  def to_s
+    
+  end
+
+end
