f43cb878ed351bafee74d090f4ea8240ff8de531
Jordan Maguire <jmaguire@thefrontiergroup.com.au>
2016-03-14 23:09:37 +0800

    Render controller prefixes in the form header

--- spec/lib/frontier/form_header_spec.rb
+++ spec/lib/frontier/form_header_spec.rb
@@ -9,18 +9,33 @@ describe Frontier::FormHeader do
     subject { form_header.to_s }
     let(:attributes) do
       {
-        model: {namespaces: namespaces}
+        model: {
+          controller_prefixes: controller_prefixes
+        }
       }
     end
+    let(:controller_prefixes) { nil }
 
-    context "when there are no namespaces" do
-      let(:namespaces) { nil }
+    context "when there are no namespaces or nested models" do
       it { should eq("simple_form_for @model, wrapper: \"horizontal\", html: {class: \"form-horizontal\"} do |f|") }
     end
 
     context "when there are namespaces" do
-      let(:namespaces) { [:namespace] }
+      let(:controller_prefixes) { [:namespace] }
+
       it { should eq("simple_form_for [:namespace, @model], wrapper: \"horizontal\", html: {class: \"form-horizontal\"} do |f|") }
+    end
+
+    context "when there are nested models" do
+      let(:controller_prefixes) { ["@nested_model", "@other_model"] }
+
+      it { should eq("simple_form_for [@nested_model, @other_model, @model], wrapper: \"horizontal\", html: {class: \"form-horizontal\"} do |f|") }
+    end
+
+    context "when there are a mix of namespaces and nested models" do
+      let(:controller_prefixes) { [:admin, "@nested_model", "@other_model"] }
+
+      it { should eq("simple_form_for [:admin, @nested_model, @other_model, @model], wrapper: \"horizontal\", html: {class: \"form-horizontal\"} do |f|") }
     end
 
   end
