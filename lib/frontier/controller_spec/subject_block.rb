1d95707722d408066f91cb05d0d52832e69ac869
Jordan Maguire <jmaguire@thefrontiergroup.com.au>
2016-03-30 21:04:59 +0800

    Support nested objects in controller spec subject blocks

--- lib/frontier/controller_spec/subject_block.rb
+++ lib/frontier/controller_spec/subject_block.rb
@@ -10,7 +10,44 @@ class Frontier::ControllerSpec::SubjectBlock
   end
 
   def to_s
-    "subject { #{method} :#{action} }"
+    [
+      "subject { #{subject_contents} }",
+      nested_model_setup
+    ].reject(&:blank?).join("\n")
+  end
+
+private
+
+  def nested_model_prefixes
+    model_configuration.controller_prefixes.select(&:nested_model?).sort_by(&:as_snake_case)
+  end
+
+  def nested_model_params
+    nested_params = {}
+    nested_model_prefixes.each do |controller_prefix|
+      nested_params["#{controller_prefix.as_snake_case}_id"] = "#{controller_prefix.as_snake_case}.id"
+    end
+    nested_params
+  end
+
+  # For a model that is nested, we need to ensure that the objects that
+  # the model is nested under are provided. EG:
+  #
+  #   subject { get :index, user_id: user.id }
+  #   let(:user) { FactoryGirl.create(:user) }
+  #
+  def nested_model_setup
+    nested_model_prefixes.map do |controller_prefix|
+      factory_statement = Frontier::FactoryGirlSupport::Declaration.new("create", controller_prefix.as_snake_case).to_s
+      Frontier::SpecSupport::LetStatement.new(controller_prefix.as_snake_case, factory_statement)
+    end.join("\n")
+  end
+
+  def subject_contents
+    [
+      "#{method} :#{action}",
+      Frontier::HashSingleLineDecorator.new(nested_model_params.merge(params)).to_s,
+    ].reject(&:empty?).join(", ")
   end
 
 end
--- spec/lib/frontier/controller_spec/subject_block_spec.rb
+++ spec/lib/frontier/controller_spec/subject_block_spec.rb
@@ -23,6 +23,35 @@ describe Frontier::ControllerSpec::SubjectBlock do
       it { should eq("subject { get :index }") }
     end
 
+    context "with some additional arguments" do
+      let(:model_configuration) { build_model_configuration }
+      let(:params) { {user: "attributes"} }
+
+      it { should eq("subject { get :index, user: attributes }") }
+    end
+
+    context "with some nested models" do
+      let(:model_configuration) do
+        Frontier::ModelConfiguration.new({
+          model_name: {
+            controller_prefixes: ["@user", "@cat"]
+          }
+        })
+      end
+      let(:params) { {user: "attributes"} }
+
+      let(:expected) do
+        raw = <<STRING
+subject { get :index, cat_id: cat.id, user_id: user.id, user: attributes }
+let(:cat) { FactoryGirl.create(:cat) }
+let(:user) { FactoryGirl.create(:user) }
+STRING
+        raw.rstrip
+      end
+
+      it { should eq(expected) }
+    end
+
   end
 
 end
