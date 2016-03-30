375f88c12e4286fab69ac0505864b8a3047618d6
Jordan Maguire <jmaguire@thefrontiergroup.com.au>
2016-03-30 21:04:38 +0800

    FactoryGirl declaration now supports a string as the key

--- spec/lib/frontier/factory_girl_support/declaration_spec.rb
+++ spec/lib/frontier/factory_girl_support/declaration_spec.rb
@@ -3,15 +3,15 @@ require 'spec_helper'
 RSpec.describe Frontier::FactoryGirlSupport::Declaration do
 
   describe '#to_s' do
-    subject { Frontier::FactoryGirlSupport::Declaration.new("build", model_or_association).to_s }
+    subject { Frontier::FactoryGirlSupport::Declaration.new("build", factory_object).to_s }
 
     context "with a Frontier::ModelConfiguration" do
-      let(:model_or_association) { build_model_configuration }
+      let(:factory_object) { build_model_configuration }
       it { should eq("FactoryGirl.build(:test_model)") }
     end
 
     context "with a Frontier::Association" do
-      let(:model_or_association) { Frontier::Association.new(build_model_configuration, name, options) }
+      let(:factory_object) { Frontier::Association.new(build_model_configuration, name, options) }
       let(:name) { "association_name" }
       let(:options) { {class_name: class_name} }
 
@@ -24,7 +24,11 @@ RSpec.describe Frontier::FactoryGirlSupport::Declaration do
         let(:class_name) { "new_class_name" }
         it { should eq("FactoryGirl.build(:new_class_name)") }
       end
+    end
 
+    context "with a string" do
+      let(:factory_object) { "yolotronix" }
+      it { should eq("FactoryGirl.build(:yolotronix)") }
     end
   end
 
