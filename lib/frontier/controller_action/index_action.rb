afb9eedc585d83cd14f147e9201e44af4c78d556
Jordan Maguire <jmaguire@thefrontiergroup.com.au>
2016-04-12 11:52:26 +0800

    Generate the @ransack_query in the controller

--- lib/frontier/controller_action/index_action.rb
+++ lib/frontier/controller_action/index_action.rb
@@ -6,13 +6,36 @@ class Frontier::ControllerAction::IndexAction
     raw = <<-STRING
 def index
   #{Frontier::Authorization::Assertion.new(model_configuration, :index).to_s}
-  #{model_configuration.as_ivar_collection} = #{scopable_object}.page(params[:page])
+#{Frontier::RubyRenderer.new(object_scope).render(1)}
 end
 STRING
     raw.rstrip
   end
 
 private
+
+  def sortable?
+    model_configuration.attributes.any?(&:sortable?)
+  end
+
+  def object_scope
+    if sortable?
+      ransack_query = "@ransack_query = #{model_configuration.as_constant}.ransack(params[:q])"
+      merged_query = "#{model_configuration.as_ivar_collection} = #{scopable_object}.merge(@ransack_query.result)"
+
+      # The leading whitespace will allow the second line of the query to line up with the first
+      leading_whitespace = Array.new(merged_query.split(".").first.length, " ").join("")
+      pagination  = "#{leading_whitespace}.page(params[:page])"
+
+      [
+        ransack_query,
+        merged_query,
+        pagination
+      ].join("\n")
+    else
+      "#{model_configuration.as_ivar_collection} = #{scopable_object}.page(params[:page])"
+    end
+  end
 
   def scopable_object
     Frontier::ControllerActionSupport::ScopableObject.new(model_configuration).to_s
