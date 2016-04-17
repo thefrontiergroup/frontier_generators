db8351f60d73fc10222979b977782d7d708f98d6
Jordan Maguire <jmaguire@thefrontiergroup.com.au>
2016-04-17 22:28:09 +0800

    Bug fix: #sortable?

--- lib/generators/frontier_crud_views/templates/sort_index_spec.rb
+++ lib/generators/frontier_crud_views/templates/sort_index_spec.rb
@@ -4,7 +4,7 @@ feature 'Admin can sort an index of <%= model_configuration.model_name.pluralize
 
   sign_in_as(:admin)
 
-<% model_configuration.attributes.select(&:sortable).sort_by(&:name).each do |attribute| %>
+<% model_configuration.attributes.select(&:sortable?).sort_by(&:name).each do |attribute| %>
 <%= render_with_indent(2, Frontier::FeatureSpec::SortScenario.new(attribute)).to_s %>
 
 <% end %>
