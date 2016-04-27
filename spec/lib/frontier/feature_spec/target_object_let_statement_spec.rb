1acb7afc8fa098d3e137358682e5d9bd4e11831b
Jordan Maguire <jmaguire@thefrontiergroup.com.au>
2016-04-27 15:25:37 +0800

    Fix #132: Feature specs generate nested resources correctly

--- spec/lib/frontier/feature_spec/target_object_let_statement_spec.rb
+++ spec/lib/frontier/feature_spec/target_object_let_statement_spec.rb
@@ -3,10 +3,109 @@ require 'spec_helper'
 describe Frontier::FeatureSpec::TargetObjectLetStatement do
 
   describe "#to_s" do
-    subject { Frontier::FeatureSpec::TargetObjectLetStatement.new(model_configuration).to_s }
-    let(:model_configuration) { Frontier::ModelConfiguration.new({model_name: {}}) }
+    let(:let_statement) { Frontier::FeatureSpec::TargetObjectLetStatement.new(model_configuration) }
 
-    it { should eq("let!(:model_name) { FactoryGirl.create(:model_name) }") }
+    describe "with include_resource: true" do
+      subject { let_statement.to_s }
+
+      context "with no nested models" do
+        let(:model_configuration) { Frontier::ModelConfiguration.new({model_name: {}}) }
+        it { should eq("let!(:model_name) { FactoryGirl.create(:model_name) }") }
+      end
+
+      context "with a nested model" do
+        let(:model_configuration) do
+          Frontier::ModelConfiguration.new({
+            model_name: {
+              controller_prefixes: ["@cat"]
+            }
+          })
+        end
+        let(:params) { {user: "attributes"} }
+
+        let(:expected) do
+          raw = <<STRING
+let!(:model_name) { FactoryGirl.create(:model_name, cat: cat) }
+let(:cat) { FactoryGirl.create(:cat) }
+STRING
+          raw.rstrip
+        end
+
+        it { should eq(expected) }
+      end
+
+      context "with some nested models" do
+        let(:model_configuration) do
+          Frontier::ModelConfiguration.new({
+            model_name: {
+              controller_prefixes: ["@user", "@cat"]
+            }
+          })
+        end
+        let(:params) { {user: "attributes"} }
+
+        let(:expected) do
+          raw = <<STRING
+let!(:model_name) { FactoryGirl.create(:model_name, cat: cat) }
+let(:cat) { FactoryGirl.create(:cat, user: user) }
+let(:user) { FactoryGirl.create(:user) }
+STRING
+          raw.rstrip
+        end
+
+        it { should eq(expected) }
+      end
+    end
+
+    describe "with include_resource: false" do
+      subject { let_statement.to_s(include_resource: false) }
+
+      context "with no nested models" do
+        let(:model_configuration) { Frontier::ModelConfiguration.new({model_name: {}}) }
+        it { should eq("") }
+      end
+
+      context "with a nested model" do
+        let(:model_configuration) do
+          Frontier::ModelConfiguration.new({
+            model_name: {
+              controller_prefixes: ["@cat"]
+            }
+          })
+        end
+        let(:params) { {user: "attributes"} }
+
+        let(:expected) do
+          raw = <<STRING
+let(:cat) { FactoryGirl.create(:cat) }
+STRING
+          raw.rstrip
+        end
+
+        it { should eq(expected) }
+      end
+
+      context "with some nested models" do
+        let(:model_configuration) do
+          Frontier::ModelConfiguration.new({
+            model_name: {
+              controller_prefixes: ["@user", "@cat"]
+            }
+          })
+        end
+        let(:params) { {user: "attributes"} }
+
+        let(:expected) do
+          raw = <<STRING
+let(:cat) { FactoryGirl.create(:cat, user: user) }
+let(:user) { FactoryGirl.create(:user) }
+STRING
+          raw.rstrip
+        end
+
+        it { should eq(expected) }
+      end
+    end
   end
 
 end
