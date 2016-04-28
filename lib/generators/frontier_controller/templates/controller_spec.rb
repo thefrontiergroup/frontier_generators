0f7ea8826e6956eff3b91cce30acd8d5ca1a1218
Jordan Maguire <jmaguire@thefrontiergroup.com.au>
2016-04-28 16:47:33 +0800

    Wrap create spec in object

--- lib/generators/frontier_controller/templates/controller_spec.rb
+++ lib/generators/frontier_controller/templates/controller_spec.rb
@@ -15,45 +15,7 @@ describe <%= controller_name %> do
 <% if model_configuration.show_update? -%>
 <%= render_with_indent(1, Frontier::ControllerSpec::EditAction.new(model_configuration).to_s) %>
 
-  describe 'POST update' do
-<%= render_with_indent(2, Frontier::ControllerSpec::SubjectBlock.new(model_configuration, :post, :update, {id: "#{model_configuration.model_name}.id", model_configuration.model_name => "attributes"}).to_s) %>
-
-    let(:attributes) { {} }
-    let(<%= model_configuration.as_symbol %>) { FactoryGirl.create(<%= model_configuration.as_symbol %>) }
-
-    authenticated_as(:admin) do
-
-      context "with valid parameters" do
-<%= render_with_indent(4, Frontier::SpecSupport::ObjectSetup.new(model_configuration).to_s) %>
-
-        it "updates the <%= model_configuration.as_constant %> object with the given attributes" do
-          subject
-
-          <%= model_configuration.model_name %>.reload
-<%= render_with_indent(5, Frontier::SpecSupport::ObjectAttributesAssertion.new(model_configuration).to_s) %>
-        end
-
-        it { should redirect_to(<%= model_configuration.url_builder.index_path(show_nested_model_as_ivar: false) %>) }
-
-        it "sets a notice for the user" do
-          subject
-          expect(flash[:notice]).to be_present
-        end
-      end
-
-      context "with invalid parameters" do
-        let(:attributes) { parameters_for(<%= model_configuration.as_symbol %>, :invalid) }
-
-        it "doesn't update the <%= model_configuration.as_constant %>" do
-          subject
-          expect(<%= model_configuration.model_name %>.reload).not_to have_attributes(attributes)
-        end
-      end
-    end
-
-    it_behaves_like "action requiring authentication"
-    it_behaves_like "action authorizes roles", [:admin]
-  end
+<%= render_with_indent(1, Frontier::ControllerSpec::UpdateAction.new(model_configuration).to_s) %>
 
 <% end -%>
 <% if model_configuration.show_delete? -%>
---
+++ spec/lib/frontier/controller_spec/update_action_spec.rb
@@ -0,0 +1,73 @@
+require 'spec_helper'
+
+describe Frontier::ControllerSpec::UpdateAction do
+
+  describe "#to_s" do
+    subject { index_action.to_s }
+    let(:index_action) { Frontier::ControllerSpec::UpdateAction.new(model_configuration) }
+    let(:model_configuration) do
+      Frontier::ModelConfiguration.new({
+        user: {
+          controller_prefixes: ["@company"],
+          attributes: {
+            name: {type: "string"}
+          }
+        }
+      })
+    end
+
+    let(:expected) do
+      raw = <<STRING
+describe 'POST update' do
+  subject { post :update, company_id: company.id, id: user.id, user: attributes }
+  let(:company) { FactoryGirl.create(:company) }
+
+  let(:attributes) { {} }
+  let(:user) { FactoryGirl.create(:user) }
+
+  authenticated_as(:admin) do
+
+    context "with valid parameters" do
+      let(:user_attributes) { FactoryGirl.attributes_for(:user) }
+      let(:attributes) do
+        {
+          name: user_attributes[:name]
+        }
+      end
+
+      it "updates the User object with the given attributes" do
+        subject
+
+        user.reload
+        expect(user.name).to eq(user_attributes[:name])
+      end
+
+      it { should redirect_to(company_users_path(company)) }
+
+      it "sets a notice for the user" do
+        subject
+        expect(flash[:notice]).to be_present
+      end
+    end
+
+    context "with invalid parameters" do
+      let(:attributes) { parameters_for(:user, :invalid) }
+
+      it "doesn't update the User" do
+        subject
+        expect(user.reload).not_to have_attributes(attributes)
+      end
+    end
+  end
+
+  it_behaves_like "action requiring authentication"
+  it_behaves_like "action authorizes roles", [:admin]
+end
+STRING
+      raw.rstrip
+    end
+
+    it { should eq(expected) }
+  end
+
+end
